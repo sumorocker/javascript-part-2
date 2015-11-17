8766a652f7604997c5cf005278cdba84f7c2170b
sumorocker <macdonald.allan@live.com>
2015-11-17 04:11:17 +0000

    added finding the square root of a number in an array of objects

--- functions.js
+++ functions.js
@@ -72,7 +72,7 @@ console.log(longestWord(arrayOfPeople));
 console.log(alphaEmails(arrayOfPeople));
     
 
-//determining square of multiple elements of an array using map function
+//determining square of multiple elements of an array using map function Question 3
 
 
 var numbers = [2,4,6,8];
@@ -83,5 +83,39 @@ function squareNum (numberToBeSquared){
 }
 
 console.log(mappedNum);
+
+//determining square of an array of objects and re-inserting it into the object Question 4
+
+
+var perp1 = {
+    name: "Steve",
+    num: 1443
+}
+
+var perp2 = {
+    name: "Bling",
+    num: 1239
+}
+    
+var perp3 = {
+    name: "Trumpet",
+    num: 9308
+}
+
+var perps = [perp1, perp2, perp3]
+
+function squaredNum(numbersToBeSquared){
+    return numbersToBeSquared.num * numbersToBeSquared.num;
+};
+
+function squaredNum(numbersToBeSquared){
+	numbersToBeSquared.num = numbersToBeSquared.num * numbersToBeSquared.num;
+	return numbersToBeSquared;
+}
+
+var squaredPerps = perps.map(squaredNum);
+
+console.log(squaredPerps);
+
 
 
