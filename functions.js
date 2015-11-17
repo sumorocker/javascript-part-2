7c0f16e8c64831239e227637c6f792c245936fb9
sumorocker <macdonald.allan@live.com>
2015-11-17 04:04:41 +0000

    added longest name and alphabetized email

--- functions.js
+++ functions.js
@@ -15,3 +15,63 @@ function longestWord (arrayOfWords){
 console.log(longestWord(["Nora","Allan"]));    
 
 
+//longest name and alphabetized email addresses Question 2
+
+
+var name1 = {
+        name : "Span",
+        email: "spantheman@hotmail.com"
+        };
+
+var name2 = {
+    name : "Stanley",
+    
+    email: "stantheman@hotmail.com"
+    };
+
+var name3 = {
+    name : "Susie",
+    email: "susieQ@hotmail.com"
+    };
+
+var arrayOfPeople =[
+     name1,
+     name2,
+     name3
+    ];
+
+function longestWord(arrayOfPeople){
+    var sortedArrayByLength = arrayOfPeople.sort(
+         function(a,b){
+
+            if (a.name.length < b.name.length){
+                return -1;
+            }        
+            else {
+                return 1;
+            }
+        });
+    
+    return sortedArrayByLength;    
+}
+
+function alphaEmails (arrayOfPeople){
+    var sortedArrayByLetter = arrayOfPeople.sort(
+        function(a,b){
+            if (a.email < b.email){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        });
+    return sortedArrayByLetter;
+}
+
+console.log(longestWord(arrayOfPeople));
+console.log(alphaEmails(arrayOfPeople));
+    
+
+
+
+
