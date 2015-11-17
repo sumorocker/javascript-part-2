db5f6f8f9be5dd8bc1b27c21344e6c15fff1e228
sumorocker <macdonald.allan@live.com>
2015-11-17 03:58:24 +0000

    added find longest word in array

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
-function random(something){
-    console.log(random)
-};
+//find the longest word in an array of words and return it, Question 1
+
+function longestWord (arrayOfWords){
+     var sortedArray = arrayOfWords.sort(
+         function(a,b){
+            if (a.length >= b.length){
+                return -1;
+            }        
+            else {
+                return 1;
+            }
+        });
+    return sortedArray[0];    
+}
+console.log(longestWord(["Nora","Allan"]));    
+
+
